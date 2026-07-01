c8712cc7d3c9d46d449cf59602e04d25cc0fad40
Neha Sethiya <nehalnct21@gmail.com>
2026-07-02 02:30:31 +0530

    Implement palindrome check, vowel count, and frequency
    Added functions to check for palindromes, count vowels, and calculate letter frequency.

--- Modules/name_modules.py
+++ Modules/name_modules.py
@@ -1,1 +1,27 @@
+def ispalindrome(name):
+    check_name = name.replace(" ", "")
 
+    if check_name == check_name[::-1]:
+        return "Yes it is a palindrome."
+    else:
+        return "No it is not a palindrome."
+
+def count_the_vowels(name):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for ch in name:
+        if ch in vowels:
+            count += 1
+    return count
+
+
+def frequency_of_letters(name):
+    frequency = {}
+
+    for ch in name:
+        if ch != " ":
+            if ch in frequency:
+                frequency[ch] += 1
+            else:
+                frequency[ch] = 1
+    return frequency
